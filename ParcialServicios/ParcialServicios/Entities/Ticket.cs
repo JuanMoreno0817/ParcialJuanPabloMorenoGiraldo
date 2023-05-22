4a4ef932d7e8cb5883cff53c7cd18a6602abfa6d
juanmoreno0817 <juanelmorenito13@hotmail.com>
2023-05-21 19:19:38 -0500

    New WebApi Project with new Ticket entity

---
+++ ParcialServicios/ParcialServicios/Entities/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialServicios.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime UseDate { get; set; }
+        public Boolean IsUsed { get; set; }
+        [Display(Name = "Entrada")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe de tener como máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string EntranceGate { get; set; }
+    }
+}
