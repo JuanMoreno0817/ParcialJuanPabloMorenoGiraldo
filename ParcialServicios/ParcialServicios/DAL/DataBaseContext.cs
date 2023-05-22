c0eee23db4e102304709e2c2464fad003f990e37
juanmoreno0817 <juanelmorenito13@hotmail.com>
2023-05-21 21:14:56 -0500

    Add initial migration and DataBaseContext with his ConnString for the new data base

---
+++ ParcialServicios/ParcialServicios/DAL/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcialServicios.DAL.Entities;
+
+namespace ParcialServicios.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options ): base(options) 
+        {
+            
+        }
+        
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t => t.Id).IsUnique();
+        }
+    }
+}
--- ParcialServicios/ParcialServicios/DAL/Entities/Ticket.cs
+++ ParcialServicios/ParcialServicios/DAL/Entities/Ticket.cs
@@ -1,16 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace ParcialServicios.Entities
+namespace ParcialServicios.DAL.Entities
 {
     public class Ticket
     {
         [Key]
         public Guid Id { get; set; }
-        public DateTime UseDate { get; set; }
-        public Boolean IsUsed { get; set; }
+        public DateTime? UseDate { get; set; }
+        public bool IsUsed { get; set; }
         [Display(Name = "Entrada")]
         [MaxLength(10, ErrorMessage = "El campo {0} debe de tener como máximo {1} caracteres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public string EntranceGate { get; set; }
+        public string? EntranceGate { get; set; }
     }
 }
